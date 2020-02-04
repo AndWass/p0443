0c9e9f750635e1c0d5d4fc4916d8ad6da40427ea
Andreas Wass <wass.andreas@gmail.com>
2020-02-04 20:21:42 +0100

    Add license comment headers

--- include/p0443_v2/asio/executor.hpp
+++ include/p0443_v2/asio/executor.hpp
@@ -1,3 +1,9 @@
+
+//          Copyright Andreas Wass 2004 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <p0443_v2/type_traits.hpp>
--- samples/asio/tcp_echo/tcp_echo_client.cpp
+++ samples/asio/tcp_echo/tcp_echo_client.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright Andreas Wass 2004 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/utility/string_view.hpp>
