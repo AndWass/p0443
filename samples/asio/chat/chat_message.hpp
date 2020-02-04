01b25d2d19d6b4f1d101690c331c88725594561f
Andreas Wass <wass.andreas@gmail.com>
2020-02-04 19:13:27 +0100

    Fix chat_message namespace usage

--- samples/asio/chat/chat_message.hpp
+++ samples/asio/chat/chat_message.hpp
@@ -4,6 +4,8 @@
 #include <p0443_v2/asio/read_all.hpp>
 #include <p0443_v2/then.hpp>
 
+#include <boost/asio/buffer.hpp>
+
 #include <string>
 #include <cstdio>
 
@@ -20,17 +22,17 @@ struct chat_message
         char header[5] = "";
         std::snprintf(header, 5, "%4d", static_cast<int>(message_.size()));
         message_.insert(0, header);
-        return p0443_v2::asio::write_all(socket, net::buffer(message_));
+        return p0443_v2::asio::write_all(socket, boost::asio::buffer(message_));
     }
 
     template <class Socket>
     auto read(Socket &socket) {
         message_.resize(4);
-        auto read_header = p0443_v2::asio::read_all(socket, net::buffer(message_));
+        auto read_header = p0443_v2::asio::read_all(socket, boost::asio::buffer(message_));
         auto read_body = [this, &socket](std::size_t /*header_read_amount = 4*/) {
             auto body_size = std::atoi(message_.c_str());
             message_.resize(body_size);
-            return p0443_v2::asio::read_all(socket, net::buffer(message_));
+            return p0443_v2::asio::read_all(socket, boost::asio::buffer(message_));
         };
 
         return p0443_v2::then(std::move(read_header), std::move(read_body));
