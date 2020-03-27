7dccc625496002e5987b2c0e204b1743e18da5a9
Andreas Wass <wass.andreas@gmail.com>
2020-03-27 21:40:02 +0100

    Update conanfile

--- conanfile.py
+++ conanfile.py
@@ -6,23 +6,17 @@ class P0443Conan(ConanFile):
     default_options = {"build_tests": False}
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    version = "0.0.4b"
+    version = "0.0.4g"
     license = "BSL-1.0"
     exports_sources = "include/*", "LICENSE"
     no_copy_source = True
 
-    def configure(self):        
+    def requirements(self):
         if self.options.build_tests:
             self.requires("doctest/2.3.5")
             self.requires("boost/[>=1.71]@conan/stable")
             self.generators.append("cmake")
-        
-    def build(self):
-        if self.options.build_tests:
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.build()
-    
+
     def package(self):
         self.copy("*.hpp")
         self.copy("LICENSE", dst="licenses")
